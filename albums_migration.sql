6df93ec6acef175228aa37bda6aafcc71ec44ddc
Boryana1 <boryana.dimova1@gmail.com>
2022-06-10 13:53:01 -0500

    chenged from artist to sales to NOT NULL

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,15 @@
+create database if not exists codeup_test_db;
 USE codeup_test_db;
+
 CREATE TABLE IF NOT EXISTS albums (
-                                      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                      artist VARCHAR(255)  DEFAULT 'NONE',
-                                      name VARCHAR(255) DEFAULT 'Anonymous',
-                                      release_date INT NOT NULL,
-                                      genre VARCHAR(255),
-                                      sales float,
+                                      id INT UNSIGNED   UNIQUE AUTO_INCREMENT,
+                                      artist VARCHAR(255)  NOT NULL ,
+                                      name VARCHAR(255) NOT NULL ,
+                                      release_date INT UNSIGNED NOT NULL,
+                                      genre VARCHAR(255) NOT NULL ,
+                                      sales DECIMAL UNSIGNED NOT NULL,
                                       PRIMARY KEY (id)
 );
 
+USE codeup_test_db;
 DESCRIBE albums;
