1f5d365b08d3f8a063af3c8b7de64c99a1d90e55
Boryana1 <boryana.dimova1@gmail.com>
2022-06-10 13:54:35 -0500

    The year Sgt. Pepper's Lonely Hearts Club Band was released

---
+++ select_exercises.sql
@@ -0,0 +1,13 @@
+create database if not exists codeup_test_db;
+use codeup_test_db;
+
+-- SELECT The name of all albums by Pink Floyd.
+
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+-- SELECT The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_date FROM albums WHERE artist = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+-- SELECT The genre for Nevermind
+-- SELECT Which albums were released in the 1990s
+-- SELECT Which albums had less than 20 million certified sales
+-- SELECT All the albums with a genre of "Rock".
