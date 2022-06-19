1bc166896a3055bb69f5605b5aa3c0862edce2a5
Boryana1 <boryana.dimova1@gmail.com>
2022-06-19 15:56:48 -0500

    done

--- select_exercises.sql
+++ select_exercises.sql
@@ -10,10 +10,10 @@ SELECT release_date FROM albums WHERE album_name = 'Sgt. Pepper''s Lonely Hearts
 SELECT genre FROM albums WHERE album_name = 'Nevermind';
 
 -- SELECT Which albums were released in the 1990s
-SELECT album_name FROM albums WHERE release_date LIKE '199%';
+SELECT * FROM albums WHERE release_date LIKE '199%';
 
 -- SELECT Which albums had less than 20 million certified sales
-SELECT album_name FROM albums WHERE  sales < 20;
+SELECT * FROM albums WHERE  sales < 20;
 
 -- SELECT All the albums with a genre of "Rock".
-SELECT album_name FROM albums WHERE genre = 'Rock';
+SELECT * FROM albums WHERE genre = 'Rock';
