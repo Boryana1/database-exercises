c80e3682a276ce2ffd461775ccf64426d0564d34
Boryana1 <boryana.dimova1@gmail.com>
2022-06-19 14:00:58 -0500

    done

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,15 +1,19 @@
--- create database if not exists codeup_test_db;
 use codeup_test_db;
 
 -- SELECT The name of all albums by Pink Floyd.
-
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT album_name FROM albums WHERE artist = 'Pink Floyd';
 
 -- SELECT The year Sgt. Pepper's Lonely Hearts Club Band was released
-
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date FROM albums WHERE album_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 -- SELECT The genre for Nevermind
+SELECT genre FROM albums WHERE album_name = 'Nevermind';
+
 -- SELECT Which albums were released in the 1990s
+SELECT album_name FROM albums WHERE release_date LIKE '199%';
+
 -- SELECT Which albums had less than 20 million certified sales
+SELECT album_name FROM albums WHERE  sales < 20;
+
 -- SELECT All the albums with a genre of "Rock".
+SELECT album_name FROM albums WHERE genre = 'Rock';
