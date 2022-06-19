16f777eacafe501c81b70dec989f01f185f35759
Boryana1 <boryana.dimova1@gmail.com>
2022-06-19 16:47:54 -0500

    done

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -2,10 +2,15 @@ USE codeup_test_db;
 
 # Write SELECT statements for:
 # Albums released after 1991
-SELECT * FROM albums WHERE release_date > 1991;
+-- SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 # Albums with the genre 'disco'
-SELECT * FROM albums WHERE genre = 'disco';
+-- SELECT * FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
 
-#     Albums by 'The Beatles'
-SELECT * FROM albums WHERE artist = 'The Beatles';
+#     Albums by 'Dire Straits'
+-- SELECT * FROM albums WHERE artist = 'Dire Straits';
+DELETE FROM albums WHERE artist = 'Dire Straits';
+
+SELECT * FROM albums;
